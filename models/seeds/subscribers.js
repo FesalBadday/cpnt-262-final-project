22e37d5b9497386d3667d1c9dfd776c0ab92dae4
FesalBadday <fesal.n04@hotmail.com>
2021-11-13 14:25:35 -0700

    added platform

--- models/seeds/subscribers.js
+++ models/seeds/subscribers.js
@@ -1,23 +1,27 @@
 module.exports = [{
   name: 'Fesal Badday',
   email: 'fesal@email.com',
+  platform: 'pc',
   dateSigned: new Date().toISOString().split('T')[0]
 },
 
 {
   name: 'Lucas Cummings',
   email: 'lucas@email.com',
+  platform: 'playstation',
   dateSigned: new Date().toISOString().split('T')[0]
 },
 
 {
   name: 'Raj Hunjan',
   email: 'raj@email.com',
+  platform: 'xbox',
   dateSigned: new Date().toISOString().split('T')[0]
 },
 
 {
   name: 'Hyeju Helena Hong',
   email: 'hyeju@email.com',
+  platform: 'nintendo-switch',
   dateSigned: new Date().toISOString().split('T')[0]
 }]
