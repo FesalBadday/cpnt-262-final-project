24d5d12a9082c037f3b4a4691b54a89112e7fcc5
FesalBadday <fesal.n04@hotmail.com>
2021-11-13 11:04:52 -0700

    updated img link path

--- models/seeds/store.js
+++ models/seeds/store.js
@@ -4,7 +4,7 @@ module.exports = [{
   id: 1,
   title: 'Free to Play',
   description: 'Check out what free!',
-  imageSrc: 'assets/images/sm/image1.jpg',
+  imageSrc: '/assets/images/sm/image1.jpg',
   // https://www.pexels.com/photo/person-holding-sony-ps4-controller-1174597/
   width: 200,
   height: 200
@@ -14,7 +14,7 @@ module.exports = [{
   id: 2,
   title: 'Early Access',
   description: 'Get a look at new games coming soon!',
-  imageSrc: 'assets/images/sm/image2.jpg',
+  imageSrc: '/assets/images/sm/image2.jpg',
   // https://www.pexels.com/photo/flat-screen-computer-monitor-3977908/
   width: 200,
   height: 200
@@ -24,7 +24,7 @@ module.exports = [{
   id: 3,
   title: 'Action',
   description: 'Action packed games!',
-  imageSrc: 'assets/images/sm/image3.jpg',
+  imageSrc: '/assets/images/sm/image3.jpg',
   // https://unsplash.com/photos/nmTm7knUnqs
   width: 200,
   height: 200
@@ -34,7 +34,7 @@ module.exports = [{
   id: 4,
   title: 'Adventure',
   description: 'Top selling adventure games!',
-  imageSrc: 'assets/images/sm/image4.jpg',
+  imageSrc: '/assets/images/sm/image4.jpg',
   // https://unsplash.com/photos/-HuMEdCPM0U
   width: 200,
   height: 200
@@ -44,7 +44,7 @@ module.exports = [{
   id: 5,
   title: 'Indie',
   description: 'Browse our indie collection!',
-  imageSrc: 'assets/images/sm/image5.jpg',
+  imageSrc: '/assets/images/sm/image5.jpg',
   // https://unsplash.com/photos/hzad7o11p5I
   width: 200,
   height: 200
@@ -54,7 +54,7 @@ module.exports = [{
   id: 6,
   title: 'Racing',
   description: 'Amazing racing games!',
-  imageSrc: 'assets/images/sm/image6.jpg',
+  imageSrc: '/assets/images/sm/image6.jpg',
   // https://unsplash.com/photos/HZrcqdS9IEU
   width: 200,
   height: 200
@@ -64,7 +64,7 @@ module.exports = [{
   id: 7,
   title: 'RPG',
   description: 'The best RPGs right here!',
-  imageSrc: 'assets/images/sm/image7.jpg',
+  imageSrc: '/assets/images/sm/image7.jpg',
   // https://unsplash.com/photos/Bp_-zo3Nl28
   width: 200,
   height: 200
@@ -74,7 +74,7 @@ module.exports = [{
   id: 8,
   title: 'Sports',
   description: 'Best selling sports games right here!',
-  imageSrc: 'assets/images/sm/image8.jpg',
+  imageSrc: '/assets/images/sm/image8.jpg',
   // https://unsplash.com/photos/1b6LfEIL7CY
   width: 200,
   height: 200
@@ -84,7 +84,7 @@ module.exports = [{
   id: 9,
   title: 'Strategy',
   description: 'Best strategy games for you!',
-  imageSrc: 'assets/images/sm/image9.jpg',
+  imageSrc: '/assets/images/sm/image9.jpg',
   // https://unsplash.com/photos/EHLd2utEf68
   width: 200,
   height: 200
